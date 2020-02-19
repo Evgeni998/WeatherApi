ad362fca180cad88f7bf904b5eee9b77793cb3fd
unknown <email@example.comevgenikunchev98@gmail.com>
2020-02-19 20:27:49 +0200

    Added comments

--- laravel/resources/views/weather.blade.php
+++ laravel/resources/views/weather.blade.php
@@ -22,23 +22,33 @@
         <div class = "main">
             <div class = "outer-container">
                 <div class = "inner-container">
-              
-                @for($index = 0; $index < 40; $index+=8)
-                      
+                @for($index = 0; $index < 40; $index+=8)     
                     <div class = "smh">
+                    <!-- Get the current date and transforms it into the day of the week(only the first 3 letters) -->
+                    <h4 id = "date">{{substr(date("l", strtotime($data->list[$index]->dt_txt)), 0, 3)}}</h4>
+                    <!-- Get the icon for the current state of the weather -->
                     <img src="http://openweathermap.org/img/w/{{$data->list[$index]->weather[0]->icon}}.png">
                     <div class = "degrees">
+                    <!-- Get the min and max temperature and casts it from kelvin to celsius -->
                     <p id = "d1">{{intval($data->list[$index]->main->temp_max - 273.15)}}° </p>
                     <p id = "d2">{{intval($data->list[$index]->main->temp_min - 273.15)}}°</p>
                     </div>
-                    <h4>{{date("l", strtotime($data->list[$index]->dt_txt))}}</h4>
+                    
                     
                     </div>
                 @endfor
                 </div>
             </div>
         </div>
-
+        <script>
+           
+            document.getElementsByClassName('smh')[0]
+        .addEventListener('click', function (event) {
+            alert("Hallo");
+           
+        });
+            
+        </script>
     </body>
 </html>
 
